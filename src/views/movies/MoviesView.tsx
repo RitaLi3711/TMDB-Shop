e34784d25b4984eeeb2a87baaf33f1a840239bb7
RitaLi <344021324@tdsb.on.ca>
2026-05-10 16:41:38 -0400

    Updated MoviesView

--- src/views/movies/MoviesView.tsx
+++ src/views/movies/MoviesView.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { ButtonGroup, ImageGrid, Pagination } from "@/components";
-import { IMAGE_BASE_URL, MOVIE_ENDPOINT, type MoviesResponse } from "@/core";
+import { getImageUrl, type ImageCell, MOVIE_ENDPOINT, type MoviesResponse } from "@/core";
 import { useTmdb } from "@/hooks";
 
 export const MoviesView = () => {
@@ -9,12 +9,16 @@ export const MoviesView = () => {
   const navigate = useNavigate();
   const [page, setPage] = useState(1);
   const category = interval ?? "now_playing";
-  const { data } = useTmdb<MoviesResponse>(`${MOVIE_ENDPOINT}/${category}`, { page }, [category, page]);
+  const { data } = useTmdb<MoviesResponse>(`${MOVIE_ENDPOINT}/${category}`, { page });
 
-  if (!data) return <p className="text-center text-gray-400">Loading...</p>;
+  const gridData: ImageCell[] = (data?.results ?? []).map((result) => ({
+    id: result.id,
+    imageUrl: getImageUrl(result.poster_path ?? ""),
+    primaryText: result.original_title || "",
+  }));
 
   return (
-    <section className="mx-auto max-w-[1600px] space-y-5 p-5">
+    <section className="mx-auto max-w-400 space-y-5 p-5">
       <ButtonGroup
         onClick={(value) => {
           setPage(1);
@@ -28,15 +32,14 @@ export const MoviesView = () => {
         ]}
         value={category}
       />
-      <ImageGrid
-        onClick={(id) => navigate(`/movie/${id}`)}
-        results={data.results.map((result) => ({
-          id: result.id,
-          imageUrl: `${IMAGE_BASE_URL}${result.poster_path ?? ""}`,
-          primaryText: result.original_title || "",
-        }))}
-      />
-      <Pagination maxPages={data.total_pages} onClick={setPage} page={page} />
+      {!data ? (
+        <p className="text-center text-gray-400">Loading...</p>
+      ) : (
+        <>
+          <ImageGrid images={gridData} onClick={(image: ImageCell) => navigate(`/movie/${image.id}`)} />
+          <Pagination maxPages={data.total_pages} onClick={setPage} page={page} />
+        </>
+      )}
     </section>
   );
 };
