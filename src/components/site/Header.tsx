fa0aeb4569efd504944cff35996242a06a892590
RitalI <344021324@tdsb.ca>
2026-05-16 20:59:00 -0400

    Added User and Favorite part in Header

--- src/components/site/Header.tsx
+++ src/components/site/Header.tsx
@@ -1,13 +1,18 @@
 import { useState } from "react";
+import { FaRegHeart } from "react-icons/fa";
+import { GoGear } from "react-icons/go";
 import { useLocation, useNavigate } from "react-router-dom";
 import { ButtonGroup, LinkGroup, SearchBar } from "@/components";
 import type { SearchType } from "@/core";
+import { ICON_SIZE } from "@/core";
+import { useUserContext } from "@/hooks";
 
 export const Header = () => {
   const navigate = useNavigate();
   const location = useLocation();
   const [query, setQuery] = useState<string>("");
   const [type, setType] = useState<SearchType>("movie");
+  const { userName, favorites } = useUserContext();
 
   if (location.pathname === "/") {
     return null;
@@ -50,6 +55,21 @@ export const Header = () => {
             ]}
             value={type}
           />
+        </div>
+
+        <div className="flex items-center">
+          <h1 className="mr-4 text-gray-300 text-xl">{userName}</h1>
+          <button className="relative rounded-full p-2 transition hover:bg-gray-700" onClick={() => navigate("/favorites")}>
+            <FaRegHeart size={ICON_SIZE} />
+            {favorites.size > 0 && (
+              <span className="absolute -top-1 -left-1 flex h-5 w-5 items-center justify-center rounded-full bg-blue-500 text-[10px] text-white">
+                {favorites.size}
+              </span>
+            )}
+          </button>
+          <button className="relative rounded-full p-2 transition hover:bg-gray-700" onClick={() => navigate("/settings")}>
+            <GoGear size={ICON_SIZE} />
+          </button>
         </div>
       </div>
     </header>
